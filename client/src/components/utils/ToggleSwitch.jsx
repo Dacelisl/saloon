95a726f36f5cfeb0646c39a030ee5a3a54f5ec20
Dacelisl <hero055@gmail.com>
2024-07-12 22:20:04 -0500

    toggle fix

--- client/src/components/utils/ToggleSwitch.jsx
+++ client/src/components/utils/ToggleSwitch.jsx
@@ -3,10 +3,12 @@
 const ToggleSwitch = ({ label, toggleState, handleToggleChange }) => {
   return (
     <>
-      <span className='relative text-sm '>{label}</span>
-      <label htmlFor='toggle' className='relative flex lg:text-base xxl:text-lg left-[10%] bg-stone-300 rounded-2xl h-6 w-[75%] lg:w-[80%] xl:w-[85%] mb-3'>
+      <span className='relative text-sm'>{label}</span>
+      <label htmlFor='toggle' className='relative flex items-center bg-stone-300 rounded-2xl h-6 w-full max-w-xs mb-3'>
         <input id='toggle' type='checkbox' className='hidden' checked={toggleState} onChange={handleToggleChange} />
-        <span className='slider p-0 absolute top-0 left-0 bottom-0 cursor-pointer before:right-0 before:rounded-full before:absolute before:h-6 before:w-10 before:bottom-0 before:bg-button-primary before:hover:bg-[#58ac2ea3] before:transition-all before:duration-300 before:focus:ring-gray-900 before:ease-in-out before:focus:shadow-lg sm:before:translate-x-16 md:before:translate-x-24 lg:before:translate-x-28'></span>
+        <span className={`slider p-0 absolute top-0 left-0 bottom-0 cursor-pointer rounded-full h-6 w-1/2 bg-button-primary transition-transform duration-300 ease-in-out transform ${toggleState ? 'translate-x-full' : 'translate-x-0'}`}>
+          <span className='block w-full h-full bg-button-primary hover:bg-[#58ac2ea3] rounded-full'></span>
+        </span>
       </label>
     </>
   )
